50540094a80807ae3627b63ea62a39f2bfb83765
aghufran-devops <aghufrandevops@outlook.com>
2025-09-05 17:21:32 -0700

    initial commit

---
+++ Jenkins_Server/jenkins-install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# install Jenkins
+yum update
+yum install java-21-amazon-corretto -y
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+yum install jenkins -y
+sed -i '/### Anything between here and the comment below will become the new contents of the file/a [Service]\nEnvironment="JAVA_OPTS=-Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Djava.io.tmpdir=/var/cache/jenkins/tmp/ -Dorg.apache.commons.jelly.tags.fmt.timeZone=America/Edmonton -Duser.timezone=America/Edmonton"\nEnvironment="JENKINS_OPTS=--pluginroot=/var/cache/jenkins/plugins"' /etc/systemd/system/jenkins.service.d/override.conf
+systemctl enable jenkins
+systemctl start jenkins
+
+# then install git
+yum install git -y
+
+# then Install Maven
+MAVEN_VERSION=3.9.11
+wget https://dlcdn.apache.org/maven/maven-3/$MAVEN_VERSION/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz -P /tmp
+tar -xzf /tmp/apache-maven-$MAVEN_VERSION-bin.tar.gz -C  /opt/
+mv /opt/apache-maven-$MAVEN_VERSION /opt/maven
+
+# then install terraform
+yum install -y yum-utils
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+yum -y install terraform
+
+ #finally install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
---
+++ Jenkins_Server/main.tf
@@ -0,0 +1,90 @@
+# VPC
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "igp-vpc"
+  cidr = var.vpc_cidr
+
+  azs                     = data.aws_availability_zones.azs.names
+  public_subnets          = var.public_subnets
+  map_public_ip_on_launch = true
+
+  enable_dns_hostnames = true
+
+  tags = {
+    Name        = "igp-vpc"
+    Terraform   = "true"
+    Environment = "pre"
+  }
+
+  public_subnet_tags = {
+    Name        = "jenkins-subnet"
+    Terraform   = "true"
+    Environment = "pre"
+  }
+}
+
+# SG
+module "sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "igp-jenkins-sg"
+  description = "Security Group for Jenkins Server"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      description = "HTTP"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "SSH"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  tags = {
+    Name        = "igp-jenkins-sg"
+    Terraform   = "true"
+    Environment = "pre"
+  }
+}
+
+# EC2
+module "ec2_instance" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "Jenkins-Server"
+
+  ami                         = data.aws_ami.Amazon_Linux_2.id
+  instance_type               = var.instance_type
+  availability_zone           = data.aws_availability_zones.azs.names[0]
+  subnet_id                   = module.vpc.public_subnets[0]
+  vpc_security_group_ids      = [module.sg.security_group_id]
+  key_name                    = "IGP-keypair-eks-s3-dynamo-ecr-ec2-user"
+  monitoring                  = true
+  associate_public_ip_address = true
+  user_data                   = file("jenkins-install.sh")
+
+
+  tags = {
+    Name        = "Jenkins-Server"
+    Terraform   = "true"
+    Environment = "pre"
+  }
+}
